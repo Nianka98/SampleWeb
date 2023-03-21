0ad164e25235e64a06d552f3ae4ba843001bfdaa
nilanka <nilanka@DESKTOP-F6FMHLM>
2023-03-21 17:14:54 +0530

    test

--- SmsAPI/Sms/Views/Admin/Students.aspx.cs
+++ SmsAPI/Sms/Views/Admin/Students.aspx.cs
@@ -1,25 +1,21 @@
 ﻿using Sms.Services;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace Sms.Views.Admin
 {
     public partial class Students : System.Web.UI.Page
     {
-        StudentService studentService = new StudentService();
+        private StudentService studentService = new StudentService();
+
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!IsPostBack) { getStudents(); }
         }
 
-        private void getStudents (){ 
-        StudentsList.DataSource= studentService.GetStudents();
+        private void getStudents()
+        {
+            StudentsList.DataSource = studentService.GetStudents();
             StudentsList.DataBind();
         }
     }
-
 }
